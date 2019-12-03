a628f0d129725acca1005bbad5492f4a2ea7593a
Arnoldo Gásperi <agasperi@everis.com>
2019-12-03 17:18:07 -0500

    Creación del arquetipo base

---
+++ src/main/java/com/everis/bootcamp/miprimerms/MiprimermsApplication.java
@@ -0,0 +1,13 @@
+package com.everis.bootcamp.miprimerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprimermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiprimermsApplication.class, args);
+	}
+
+}
